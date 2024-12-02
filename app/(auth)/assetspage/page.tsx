96879dee6d725815f9aa675d4c3ce9e84a1d73c2
realpantagon <pichayoothunchainao@gmail.com>
2024-12-02 14:55:27 +0700

    assets page

--- app/(auth)/assetspage/page.tsx
+++ app/(auth)/assetspage/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function AssetsPage() {
-	return <div>Assets
-	</div>;
+	return <div className="min-h-screen p-4 bg-stone-100">
+	<div className="max-w-6xl mx-auto bg-white rounded-lg shadow-md p-6">
+	  <h1 className="text-xl font-bold text-gray-800 mb-4">Assets Provide</h1>
+	  <div className="space-y-6">
+		
+	  </div>
+	</div>
+  </div>
 }
