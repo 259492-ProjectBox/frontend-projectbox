0f739de949f2480c64460296abb6120e2789dd7c
Mhing <natthap411@gmail.com>
2024-12-04 20:55:39 +0700

    update route signIn

--- app/api/signIn/route.ts
+++ app/api/signIn/route.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-import { CmuOAuthBasicInfo } from "../../../types/CmuOAuthBasicInfo";
+// import { CmuOAuthBasicInfo } from "../../../types/CmuOAuthBasicInfo";
+import axiosInstance from "@/utils/axiosInstance";
 
 type SuccessResponse = {
 	ok: true;
@@ -14,94 +14,21 @@ type ErrorResponse = {
 
 export type SignInResponse = SuccessResponse | ErrorResponse;
 
-async function getOAuthAccessTokenAsync(
-	authorizationCode: string
-): Promise<string | null> {
-	try {
-		const response = await axios.post(
-			process.env.CMU_OAUTH_GET_TOKEN_URL as string,
-			{},
-			{
-				params: {
-					code: authorizationCode,
-					redirect_uri: process.env.CMU_OAUTH_REDIRECT_URL,
-					client_id: process.env.CMU_OAUTH_CLIENT_ID,
-					client_secret: process.env.CMU_OAUTH_CLIENT_SECRET,
-					grant_type: "authorization_code",
-				},
-				headers: {
-					"content-type": "application/x-www-form-urlencoded",
-				},
-			}
-		);
-		return response.data.access_token;
-	} catch (err) {
-		console.log(err);
-		return null;
-	}
-}
-
-async function getCMUBasicInfoAsync(accessToken: string) {
-	try {
-		const response = await axios.get(
-			process.env.CMU_OAUTH_GET_BASIC_INFO as string,
-			{
-				headers: { Authorization: "Bearer " + accessToken },
-			}
-		);
-
-		return response.data as CmuOAuthBasicInfo;
-	} catch (err) {
-		console.log(err);
-		return null;
-	}
-}
-
 export async function POST(req: Request) {
 	const body = await req.json();
 	const authorizationCode = body.authorizationCode;
 
-	if (typeof authorizationCode !== "string") {
-		return NextResponse.json(
-			{ ok: false, message: "Invalid authorization code" },
-			{ status: 400 }
-		);
-	}
-
-	// Get access token
-	const accessToken = await getOAuthAccessTokenAsync(authorizationCode);
-	if (!accessToken) {
-		return NextResponse.json(
-			{ ok: false, message: "Cannot get OAuth access token" },
-			{ status: 400 }
-		);
-	}
-
-	// Get CMU basic info
-	const cmuBasicInfo = await getCMUBasicInfoAsync(accessToken);
-	if (!cmuBasicInfo) {
-		return NextResponse.json(
-			{ ok: false, message: "Cannot get CMU basic info" },
-			{ status: 400 }
-		);
-	}
-
-	if (typeof process.env.JWT_SECRET !== "string") {
-		throw new Error("Please assign JWT_SECRET in .env!");
-	}
-
-	const token = jwt.sign(
+	const tokenResponse = await axiosInstance.post(
+		"http://localhost:5000/api/signin",
 		{
-			cmuAccount: cmuBasicInfo.cmuitaccount,
-			firstName: cmuBasicInfo.firstname_EN,
-			lastName: cmuBasicInfo.lastname_EN,
-			studentId: cmuBasicInfo.student_id,
-			major: cmuBasicInfo.organization_name_EN,
-		},
-		process.env.JWT_SECRET,
-		{ expiresIn: "1h" }
+			authorizationCode: authorizationCode,
+		}
 	);
 
+	if (tokenResponse.data.ok === false) {
+		throw new Error("Cannot get token");
+	}
+	const token = tokenResponse.data.accessToken;
 	// Create response with cookie
 	const response = NextResponse.json({ ok: true });
 
