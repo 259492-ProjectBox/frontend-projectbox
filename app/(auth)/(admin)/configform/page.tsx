70c0d6d22c82b55cbc1a5e2934651b7ba912d215
realpantagon <pichayoothunchainao@gmail.com>
2024-12-17 15:39:54 +0700

    initial page config form

--- app/(auth)/(admin)/configform/page.tsx
+++ app/(auth)/(admin)/configform/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
 export default function ConfigFormPage() {
-	return <div>Config Form
-	</div>;
+
+  return (
+    <div className="min-h-screen p-4 bg-gray-100">
+      <div className="max-w-6xl mx-auto bg-white rounded-lg shadow-md p-4">
+        <h1 className="text-lg font-semibold text-gray-800 mb-3">Config Form</h1>
+        <div className="flex items-center space-x-2 mb-6">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
