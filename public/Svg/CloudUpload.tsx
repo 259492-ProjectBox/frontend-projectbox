065f80debc5bd7c0f42ce9e81d84907de4e6a294
realpantagon <pichayoothunchainao@gmail.com>
2024-12-23 20:13:07 +0700

    icon for svg

---
+++ public/Svg/CloudUpload.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const cloudUpload = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      className="h-10 w-10 text-gray-400 mb-2"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier">
+        <title>Upload-1</title>
+        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Upload-1">
+            <rect id="Rectangle" fillRule="nonzero" x="0" y="0" width="24" height="24"></rect>
+            <line
+              x1="12"
+              y1="11"
+              x2="12"
+              y2="20"
+              id="Path"
+              stroke="#0C0310"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></line>
+            <path
+              d="M15,13 L12.7071,10.7071 C12.3166,10.3166 11.6834,10.3166 11.2929,10.7071 L9,13"
+              id="Path"
+              stroke="#0C0310"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></path>
+            <path
+              d="M8,16 L6,16 C4.34315,16 3,14.6569 3,13 C3,11.3431 4.34315,10 6,10 C6,6.68629 8.68629,4 12,4 C15.3137,4 18,6.68629 18,10 C19.6569,10 21,11.3431 21,13 C21,14.6569 19.6569,16 18,16 L16,16"
+              id="Path"
+              stroke="#0C0310"
+              strokeWidth="2"
+              strokeLinecap="round"
+            ></path>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default cloudUpload;
