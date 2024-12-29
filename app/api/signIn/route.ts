6bde5d346cf526fdd7a0473bc1cd46ad4551a915
Mhing <natthap411@gmail.com>
2024-12-29 18:39:07 +0700

    integrate signIn with auth service

--- app/api/signIn/route.ts
+++ app/api/signIn/route.ts
@@ -1,6 +1,6 @@
 // import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-// import { CmuOAuthBasicInfo } from "../../../types/CmuOAuthBasicInfo";
+import { CmuOAuthBasicInfo } from "../../../types/CmuOAuthBasicInfo";
 import axiosInstance from "@/utils/axiosInstance";
 
 type SuccessResponse = {
@@ -18,17 +18,58 @@ export async function POST(req: Request) {
 	const body = await req.json();
 	const authorizationCode = body.authorizationCode;
 
-	const tokenResponse = await axiosInstance.post(
-		"http://localhost:5000/api/signin",
-		{
-			authorizationCode: authorizationCode,
-		}
-	);
-
-	if (tokenResponse.data.ok === false) {
-		throw new Error("Cannot get token");
+	if (typeof authorizationCode !== "string") {
+		return NextResponse.json(
+			{ ok: false, message: "Invalid authorization code" },
+			{ status: 400 }
+		);
 	}
+
+	
+	// Get access token
+	// const accessToken = await getOAuthAccessTokenAsync(authorizationCode);
+	// if (!accessToken) {
+	// 	return NextResponse.json(
+	// 		{ ok: false, message: "Cannot get OAuth access token" },
+	// 		{ status: 400 }
+	// 	);
+	// }
+
+	// // Get CMU basic info
+	// const cmuBasicInfo = await getCMUBasicInfoAsync(accessToken);
+	// if (!cmuBasicInfo) {
+	// 	return NextResponse.json(
+	// 		{ ok: false, message: "Cannot get CMU basic info" },
+	// 		{ status: 400 }
+	// 	);
+	// }
+
+	if (typeof process.env.JWT_SECRET !== "string") {
+		throw new Error("Please assign JWT_SECRET in .env!");
+	}
+
+	// const payload: JWTPayload = {
+    //     cmuAccount: cmuBasicInfo.cmuitaccount,
+    //     firstName: cmuBasicInfo.firstname_EN,
+    //     lastName: cmuBasicInfo.lastname_EN,
+    //     studentId: cmuBasicInfo.student_id,
+    //     orgName: cmuBasicInfo.organization_name_EN,
+    //     isAdmin: cmuBasicInfo.cmuitaccount === "admin" // or your admin check logic
+    // };
+
+    // const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+	// make it send to AuthService that is in localhost 3002 
+	// and get the response from there
+	if (typeof process.env.AUTHSERVICE_URL !== "string") {
+		throw new Error("Please assign AUTHSERVICE_URL in .env!");
+	}
+	const tokenResponse = await axiosInstance.post(process.env.AUTHSERVICE_URL + "/api/signin", {
+		authorizationCode: authorizationCode
+	});
+	
 	const token = tokenResponse.data.accessToken;
+
 	// Create response with cookie
 	const response = NextResponse.json({ ok: true });
 
