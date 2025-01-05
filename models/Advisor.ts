f44d0fe31fb2797635aa3841dc478f05ebcb6d02
realpantagon <pichayoothunchainao@gmail.com>
2025-01-05 21:48:15 +0700

    get advisor stats for major id = 1

---
+++ models/Advisor.ts
@@ -0,0 +1,11 @@
+// models/Advisor.ts
+
+export interface Advisor {
+    id: number;
+    prefix: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    major_id: number;
+  }
+  
