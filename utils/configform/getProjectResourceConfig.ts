41fa07ee052c0baa1617d054cf20b6caff0fb357
realpantagon <pichayoothunchainao@gmail.com>
2025-01-23 22:38:37 +0700

    get project resource config

---
+++ utils/configform/getProjectResourceConfig.ts
@@ -0,0 +1,20 @@
+// src/utils/apiController.ts
+import axios from "axios";
+
+export const fetchProjectResourceConfigs = async (programId: number) => {
+  try {
+    const response = await axios.get(
+      `https://project-service.kunmhing.me/api/v1/projectResourceConfigs/program/${programId}`,
+      {
+        headers: {
+          accept: "application/json",
+        },
+      }
+    );
+    console.log("get Project Resource Configs:",response.data)
+    return response.data; // Axios returns data in the `data` property of the response
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // Rethrow error to handle it in the component
+  }
+};
