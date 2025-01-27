deaa45662da7572bd9bf38562a422b4d22a57f92
realpantagon <pichayoothunchainao@gmail.com>
2025-01-27 15:02:49 +0700

    roll back no deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: "/api/:path*", // Matches /api/*
-          destination: "/.netlify/functions/:path*", // Rewrite to Netlify Functions
-        },
-        {
-          source: "/cmuOAuthCallback",
-          destination: "/cmuOAuthCallback.html", // Rewrite to specific page
-        },
-      ];
-    },
-  };
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
