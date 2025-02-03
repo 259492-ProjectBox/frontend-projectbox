8671779c6b9738668703e36386c02a95475b0823
realpantagon <pichayoothunchainao@gmail.com>
2025-02-03 15:11:53 +0700

    get student info

---
+++ utils/createproject/getStudentInfo.ts
@@ -0,0 +1,20 @@
+import { Student } from "../../models/Student";
+
+export const getStudentInfo = async (studentId: string): Promise<Student | null> => {
+  try {
+    const response = await fetch(
+      `https://project-service.kunmhing.me/api/v1/students/${studentId}`,
+      {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching student data:", error);
+    return null;
+  }
+};
