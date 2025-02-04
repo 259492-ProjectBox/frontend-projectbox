a39677f2d64015480bd270dcab6d41babcd1b4c5
realpantagon <pichayoothunchainao@gmail.com>
2025-02-04 11:26:25 +0700

    new line

--- app/(auth)/advisorprofile/[id]/page.tsx
+++ app/(auth)/advisorprofile/[id]/page.tsx
@@ -48,6 +48,7 @@ export default function AdvisorProfilePage() {
             <div className="mt-4 space-y-2">
               <p className="text-lg font-semibold">
                 {advisor.prefix_en} {advisor.first_name_en} {advisor.last_name_en}
+                <br />
                 {advisor.prefix_th} {advisor.first_name_th} {advisor.last_name_th}
               </p>
               <p className="text-sm">
