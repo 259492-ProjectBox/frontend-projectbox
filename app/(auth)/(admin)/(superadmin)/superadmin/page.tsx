6de5fc7380381f5e9574066881f5775aa9583962
realpantagon <pichayoothunchainao@gmail.com>
2025-02-06 14:12:46 +0700

    Super Admin page edit text

--- app/(auth)/(admin)/(superadmin)/superadmin/page.tsx
+++ app/(auth)/(admin)/(superadmin)/superadmin/page.tsx
@@ -389,7 +389,7 @@ export default function AdminManagePage(): JSX.Element {
           <table className="w-full text-sm text-left text-gray-500">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50">
               <tr>
-                <th scope="col" className="px-6 py-3">ID</th>
+                <th scope="col" className="px-6 py-3">No.</th>
                 <th scope="col" className="px-6 py-3">Program Name En</th>
                 <th scope="col" className="px-6 py-3">Program Name Th</th>
               </tr>
