41f30bed14aaf8907377367eed4e4cb6f8b938e5
realpantagon <pichayoothunchainao@gmail.com>
2025-02-06 14:26:08 +0700

    change api path

--- utils/dashboard/getProjectByStudentId.ts
+++ utils/dashboard/getProjectByStudentId.ts
@@ -5,7 +5,7 @@ import { Project } from "@/models/Project";
 const getProjectByStudentId = async (studentId: string): Promise<Project[]> => {
   try {
     const response = await axios.get<Project[]>(
-      `https://project-service.kunmhing.me/api/v1/projects/student/${studentId}`,
+      `https://search-service.kunmhing.me/api/v1/projects/fields?searchInput=${studentId}&fields[]=members.studentId`,
       {
         headers: { Accept: "application/json" },
       }
