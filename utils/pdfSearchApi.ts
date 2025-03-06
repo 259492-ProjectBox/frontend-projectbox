e96cdd79d797b21cd444df9a890b65b0940374c0
Mhing <natthap411@gmail.com>
2025-03-06 17:14:24 +0700

    update docker file for ci cd

--- utils/pdfSearchApi.ts
+++ utils/pdfSearchApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Project } from "@/models/SearchContentPdf";
+import { Project } from "../models/Project";
 
 export async function fetchPdfProjects(searchInput: string): Promise<Project[]> {
   try {
