55fae7193a18a7d29e52ac22fc00311af71c0f41
KunMhing <90751090+Mhingzzz@users.noreply.github.com>
2025-03-10 13:24:10 +0900

    handle login error  (#79)
    * update advisor info search by email instead and change the display in program
    
    * update handle error when cancle login

--- app/cmuEntraIDCallback/page.tsx
+++ app/cmuEntraIDCallback/page.tsx
@@ -2,18 +2,17 @@
 
 import Spinner from "@/components/Spinner";
 import { useOAuthCallback } from "@/hooks/useOAuthCallback";
+
 export default function CMUOAuthCallback() {
-	const { isLoading, error } = useOAuthCallback();
+  const { isLoading, error } = useOAuthCallback();
 
-	if (error) {
-		return <div className="p-3 text-red-500">{error}</div>;
-	}
+  if (error) {
+    return <div className="p-3 text-red-500">{error}</div>;
+  }
 
-	if (isLoading) {
-		return (
-			<Spinner/>
-		);
-	}
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-	return null;
+  return null;
 }
