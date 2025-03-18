604147be4a349dcc076dd94905fdf940664d1ab3
Mhing <natthap411@gmail.com>
2025-03-18 15:47:51 +0700

    Update .dockerignore to include .env files and import getAllKeyWord in search page

--- app/(auth)/search/page.tsx
+++ app/(auth)/search/page.tsx
@@ -13,9 +13,9 @@ import { getAcademicYears } from "@/app/api/configprogram/getAcademicYears"; // 
 import { AcademicYear } from "@/models/AcademicYear"; // Import AcademicYear
 import keywordSearchProjects, { KeywordSearchFields } from "@/app/api/search/keywordSearch"; // Import keywordSearchProjects
 import { Keyword } from "@/dtos/Keyword";
-import getAllKeyWord from "@/app/api/keywords/getAllKeyword";
 import { Autocomplete, TextField } from "@mui/material";
 import { getProgramNameById } from "@/utils/programHelpers";
+import getAllKeyWord from "@/app/api/keywords/getAllKeyWord";
 
 interface SearchFields {
   courseNo: string | null;
