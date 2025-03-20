4ee09ae5cda27983842c5f26146615981a7aeed7
Mhing <natthap411@gmail.com>
2025-03-20 10:10:08 +0700

    Update advisor email handling: decode email before API calls and rename functions for clarity

--- app/api/advisorstats/getProjectByAdvisorEmail.ts
+++ app/api/advisorstats/getProjectByAdvisorEmail.ts
@@ -4,22 +4,25 @@ import axios from "axios"
 import type { Project } from "@/models/Project"
 import { apiConfig } from "@/config/apiConfig"
 
-const getProjectsByAdvisorId = async (advisorId: string): Promise<Project[]> => {
+const getProjectByAdvisorEmail = async (advisorId: string): Promise<Project[]> => {
+  
+  const decodedEmail = decodeURIComponent(advisorId);
+  
   try {
     const response = await axios.get(
-      `${apiConfig.SearchService.ProjectSelectedFields}?fields=staffs.id&searchInputs=${advisorId}`,
+      `${apiConfig.SearchService.ProjectAllFields}?fields=staffs.email&searchInput=${decodedEmail}`,
       {
         headers: {
           accept: "*/*",
         },
       },
     )
+    
     return response.data
   } catch (error) {
-    console.error("Error in getProjectsByAdvisorId:", error)
+    console.error("Error in getProjectsByAdvisorEmail:", error)
     throw new Error("Failed to fetch projects data")
   }
 }
 
-export default getProjectsByAdvisorId
-
+export default getProjectByAdvisorEmail
